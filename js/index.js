166ae1417ac03538a7a9598f9ce014d108dc3023
shayan__CodeKing <shayanmahmood234@gmail.com>
2022-06-16 15:09:46 +0500

    heloo its completed but some bugs are left .. commit__3

---
+++ js/index.js
@@ -0,0 +1,7 @@
+document.querySelector(".btn").addEventListener("click",function () {
+ var age = prompt("What is Your Dog Age?(Plz Add Number)");
+ var ageCount = (age - 2) * 4 + 21;
+ console.log(ageCount);
+alert("Your Age IS" + " " + ageCount);
+})
+
