c2cf310fc7094408ed86afd51cb9fd99e51d3ffb
shayan__CodeKing <shayanmahmood234@gmail.com>
2022-06-16 23:47:24 +0500

    its is ready to publish commit__6

--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
-document.querySelector(".btn").addEventListener("click",function () {
- var age = prompt("What is Your Dog Age?(Plz Add Number)");
- var ageCount = (age - 2) * 4 + 21;
- console.log(ageCount);
-alert("Your Age IS" + " " + ageCount);
+
+document.querySelector(".btn").addEventListener("click", function () {
+    var age = prompt("What is Your Dog Age?(Plz Add Number)");
+    var ageCount = (age - 2) * 4 + 21;
+    console.log(ageCount);
+    alert("Your Age IS" + " " + ageCount);
+    if (ageCount > 0) {
+        document.querySelector("p").innerHTML = "Results:" + " " + "Your Age IS" + " " + age + " " + " and Your Dog Age IS" + " " + ageCount;
+        
+    }
+    else {
+        document.querySelector("p").innerHTML = "Plz Enter A Number";
+    }
+    if (ageCount > age) {
+     document.querySelector(".main--text-box--heading").textContent = "Your Age IS" + " " + age + " " + " and Your Dog Age IS" + " " + ageCount;
+    }
+    else{
+        document.querySelector(".main--text-box--heading").textContent = "Your Age IS" + " " + age + " " + " and Your Dog Age IS" + " " + ageCount;
+    }
 })
 
